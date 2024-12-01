0c81ae659a35b765845ee395acc0f7d88f89ec31
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2024-12-01 16:43:37 +0100

    IMPROVE UI

--- searchProducts.py
+++ searchProducts.py
@@ -50,7 +50,7 @@ def extract_images(soup):
         
 def searchProducts(urls):
     client = Groq(
-        api_key="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",
+        api_key="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",
     )
     products=[]
     for url in urls:
@@ -65,19 +65,19 @@ def searchProducts(urls):
             messages=[
                 {
                     "role": "user",
-                    "content": f'Read the next text and scrapp the name of the products and their prices, discount (id exists) return all as a list of product price and only food: {data} '
+                    "content": f'Read the next text and scrapp the name of the products and their prices return all as a list of product price and only food: {data} '
                         
                 }
             ],
             model="llama3-70b-8192",
         )
         text= chat_completion.choices[0].message.content
-        # print(text)
+        print(text)
         chat_completion=client.chat.completions.create(
             messages=[
                 {
                     "role": "system",
-                    "content":"Yor are a text to JSON parser the result is a JSON {products:[ {name: <description>, price: <name>, category: <category>, discount:<discoun>}}]}, you only use the data in the input to create the list, you only respond with the JSON, remove products that are not food"                        
+                    "content":"Yor are a text to JSON parser the result is a JSON {products:[ {name: <description>, price: <name>, category: <category>}}]}, you only use the data in the input to create the list, you only respond with the JSON, remove products that are not food"                        
                 },
                 {
                     "role": "user",
@@ -88,29 +88,29 @@ def searchProducts(urls):
             max_tokens=7340,
             response_format={"type": "json_object"},
         )
-        
+        print(chat_completion.choices[0].message.content)
         
         
         products_json= chat_completion.choices[0].message.content
-        images_dict = extract_images(soup)
-        images_json=json.dump({'images':images_dict},indent=2)
-        merge = chat_completion=client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content":"Yor are a text to JSON parser the result is a JSON {products:[ {name: <description>, price: <name>, category: <category>, discount:<discoun>, image:<image> alt_src:<alt_src>}}]}, you only use the data in the input to create the list, you only respond with the JSON, remove products that are not food"                        
-                },
-                {
-                    "role": "user",
-                    "content":f'using the next two json try to merge if and only if you think they can be together base on name product and image_alt :{products_json} {images_json} '       
-                }
-            ],
-            model="llama3-70b-8192",
-            max_tokens=7340,
-            # response_format={"type": "json_object"},
-        )
+        # images_dict = extract_images(soup)
+        # images_json=json.dump({'images':images_dict},indent=2)
+        # merge = chat_completion=client.chat.completions.create(
+        #     messages=[
+        #         {
+        #             "role": "system",
+        #             "content":"Yor are a text to JSON parser the result is a JSON {products:[ {name: <description>, price: <name>, category: <category>, discount:<discoun>, image:<image> alt_src:<alt_src>}}]}, you only use the data in the input to create the list, you only respond with the JSON, remove products that are not food"                        
+        #         },
+        #         {
+        #             "role": "user",
+        #             "content":f'using the next two json try to merge if and only if you think they can be together base on name product and image_alt :{products_json} {images_json} '       
+        #         }
+        #     ],
+        #     model="llama3-70b-8192",
+        #     max_tokens=7340,
+        #     # response_format={"type": "json_object"},
+        # )
 
-        print(merge)
+        # print(merge)
         r = json.loads(products_json)
         r = format(products=r['products'])
         products.extend(r)
